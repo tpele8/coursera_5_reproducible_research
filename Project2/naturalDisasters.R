c850d502e322a27e305f1c9ab043ef2b53ac64c7
tpele8 <i64425@Tyler-Peterson.local>
2016-10-11 22:47:55 -0600

    Additions to naturalDisasters code

--- Project2/naturalDisasters.R
+++ Project2/naturalDisasters.R
@@ -8,10 +8,13 @@ library(dplyr)
 # Reading in files
   setwd("/Users/i64425/GitHub/coursera_5_reproducible_research/Project2")
   df <- read.csv("repdata%2Fdata%2FStormData.csv.bz2")
+  View(df)
 
 # Explore data
-  #str(df)
+  str(df)
   
+  propdmgexpdistinct <- df %>% distinct(PROPDMGEXP)
+  propdmgexpdistinct$PROPDMGEXP
   #distinctEventType <- df %>% distinct(EVTYPE)
   #distinctEventType$EVTYPE
   
@@ -30,13 +33,32 @@ library(dplyr)
     totByEvent <- sort(tapply(dfMod1$TOTDEST, dfMod1$EVTYPE, FUN = sum), decreasing = TRUE)
     
     # Used the disasters that had at least 100 injuries/fatalities
-    totByEventReduce <- as.data.frame(totByEvent[totByEvent >= 100])
+    totByEventReduce <- head(as.data.frame(totByEvent), n = 10)
     names(totByEventReduce) <- c("MAGNITUDE")
     
-    # Plot to see how they relate
-    plot(totByEventReduce, ylab = "Magnitude Of Injuries/Fatalities", xlab = "Disaster Index", main = "Magnitude Of Disasters Across the United States")
+    # Plot to see how things relate
+    plot(totByEventReduce, ylab = "Magnitude of Injuries/Fatalities", xlab = "Disaster Index", 
+         main = "Health Impact of Top 10 Disasters Across the United States")
 
     # Print
     print(totByEventReduce)
 
 # Question 2: Across the United States, which types of events have the greatest economic consequences?
+  # Create dataframe to be used in analysis
+    dfMod2 <- df[,c(8, 25, 27)]
+    dfMod2$TOTDMG <- apply(dfMod2[,2:3], 1, FUN = sum)
+
+  # Add property damages up by each event
+    totByDmg <- sort(tapply(dfMod2$TOTDMG, dfMod2$EVTYPE, FUN = sum), decreasing = TRUE)
+
+  # Create data frame for all property damages greater than $100.00  
+    totByDmgReduce <- head(as.data.frame(totByDmg), n = 10)
+    names(totByDmgReduce) <- c("MAGNITUDE")
+
+  # Plot to see how things relate  
+    plot(totByDmgReduce, ylab = "Magnitude of Economical Damages", xlab = "Disaster Index", 
+         main = "Economic Impact From Top 10 US Disasters")
+    
+  # Print
+    print(totByDmgReduce)
+      
